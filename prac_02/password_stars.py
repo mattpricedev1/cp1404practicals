809dfd7b76c6e3a4a0c7d139db0fbe0581f1e547
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-09-23 17:25:39 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -6,8 +6,22 @@ The program should then print asterisks as long as the word.
 
 PASSWORD_MIN_LENGTH = 10
 
-password = input("Enter password: ")
-while len(password) < PASSWORD_MIN_LENGTH:
-    print(f"Password must be {PASSWORD_MIN_LENGTH} characters")
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print('*' * len(password))
+
+
+def get_password():
     password = input("Enter password: ")
-print('*' * len(password))
+    while len(password) < PASSWORD_MIN_LENGTH:
+        print(f"Password must be {PASSWORD_MIN_LENGTH} characters")
+        password = input("Enter password: ")
+    return password
+
+
+main()
