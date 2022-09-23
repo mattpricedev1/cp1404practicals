6b47096c507a7e00433e364af2f185ab158ea21d
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-09-23 16:57:32 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,13 @@
+"""
+Write a program that asks the user for a password,
+with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
+The program should then print asterisks as long as the word.
+"""
+
+PASSWORD_MIN_LENGTH = 10
+
+password = input("Enter password: ")
+while len(password) < PASSWORD_MIN_LENGTH:
+    print(f"Password must be {PASSWORD_MIN_LENGTH} characters")
+    password = input("Enter password: ")
+print('*' * len(password))
