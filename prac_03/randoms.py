f1ce2672a2e4ecc01dcbdd4d15bf43086608613c
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-03 17:37:30 +1000

    Add answers to questions

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,13 +1,21 @@
 import random
 
 # What did you see on line 1?
+# Random numbers between 5 and 20
 # What was the smallest number you could have seen, what was the largest?
+# Smallest = 5, largest = 20
 
 # What did you see on line 2?
+# Odd numbers from 3 to 10
 # What was the smallest number you could have seen, what was the largest?
+# Smallest = 3, largest = 9
 # Could line 2 have produced a 4?
+# No, as it steps in two from 3
 
 # What did you see on line 3?
+# A random floating point numbers between 2.5 and 5.5
 # What was the smallest number you could have seen, what was the largest?
+# Smallest = 2.5, largest = 5.5
 
 # Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 100))
