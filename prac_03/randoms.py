daa9dbf03fbad4ddc21fd142ed4f76a94f14948a
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-03 17:29:52 +1000

    Add questions as comments

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
