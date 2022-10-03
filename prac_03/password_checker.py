59187e9663666c5b41246fa2f7478db8f6c31d19
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-03 22:47:43 +1000

    Update character counter

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -29,7 +29,6 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
@@ -38,7 +37,6 @@ def is_valid_password(password):
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
         if char.islower():
             count_lower += 1
         elif char.isupper():
@@ -48,12 +46,13 @@ def is_valid_password(password):
         elif char in SPECIAL_CHARACTERS:
             count_special += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
+        return False
 
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
+    if SPECIAL_CHARS_REQUIRED:
+        if count_special == 0:
+            return False
 
-    # if we get here (without returning False), then the password must be valid
     return True
 
 
