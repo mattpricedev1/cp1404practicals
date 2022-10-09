4e8446960c04aa59e97e896f297593521ca9224c
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-09 16:27:11 +1000

    Add print report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,6 +13,11 @@ def main():
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
+    """Calculate and print monthly cumulative totals"""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
