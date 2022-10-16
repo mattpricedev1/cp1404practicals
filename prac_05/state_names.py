3b2910ea70daf88593f644091fa55875b8886b8d
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-16 20:25:27 +1000

    Reformat CODE_TO_NAME

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,8 +4,14 @@ State names in a dictionary
 File needs reformatting
 """
 
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland",
+                "NSW": "New South Wales",
+                "NT": "Northern Territory",
+                "WA": "Western Australia",
+                "ACT": "Australian Capital Territory",
+                "VIC": "Victoria",
+                "TAS": "Tasmania"}
+
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
