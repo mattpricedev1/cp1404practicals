78a10cf6d5217258d96e3d40e73aee4ae6a1fd9d
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-16 18:22:51 +1000

    Complete word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,14 +1,22 @@
 """
 Word Occurrences
 Estimate: 60 minutes
-Actual:
+Actual: 90 minutes
 """
 
-word_to_count = {}  # empty dictionary
+word_to_count = {}  # initialise empty dictionary
 user_string = input("Enter a string: ")
 words = user_string.split()  # convert string to list
 
 for word in words:
     count = word_to_count.get(word, 0)
-    word_to_count[word] = count + 1
-    print(f"{word} : {word_to_count[word]}")
+    word_to_count[word] = count + 1  # add word and occurrence to dictionary
+
+# display words that occur more than once only once in the list
+words = list(word_to_count.keys())
+words.sort()
+
+max_length = max(len(word) for word in words)
+
+for word in words:
+    print(f"{word:{max_length}} occurred {word_to_count[word]} time(s)")
