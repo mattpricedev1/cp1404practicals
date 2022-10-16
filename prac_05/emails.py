41b473abdb36113976b85c9cdfeeb2c9f1ec4ea0
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-16 22:24:05 +1000

    Fix email confirmation

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,10 +11,17 @@ def main():
     while email != "":
         name = get_name_from_email(email)
         confirmation = input(f"Is your name {name}? (Y/n)")
+        if confirmation != "" and confirmation.upper() != "Y":
+            name = input("Name: ")  # rename extracted name from email
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 def get_name_from_email(email):
-    """Extract name from email address"""
+    """Extract name from email"""
     prefix = email.split("@")[0]
     parts = prefix.split(".")
     name = " ".join(parts).title()
