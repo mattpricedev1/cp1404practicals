8f393f1295144d0ae44c36d2e85b989565ed6f3b
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-17 18:30:27 +1000

    Add process_records function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,17 +3,30 @@ CSV_FILE = "wimbledon.csv"
 
 def main():
     records = get_records(CSV_FILE)
+    player_to_count, countries = process_records(records)
     print(records)
 
 
 def get_records(csv_file):
     records = []
     with open(csv_file, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()
+        in_file.readline()  # skip first line
         for line in in_file:
-            row = line.strip().split(",")
-            records.append(row)
+            attribute = line.strip().split(",")  # remove commas and separate attributes
+            records.append(attribute)
         return records
+
+
+def process_records(records):
+    player_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[1])
+        try:
+            player_to_count[record[2]] += 1
+        except KeyError:
+            player_to_count[record[2]] = 1
+    return player_to_count, countries
 
 
 main()
