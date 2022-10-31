539f15bfb7426fd0254e102d99635dae7fe121fb
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-01 00:07:45 +1000

    Update fstring

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,8 +13,14 @@ def main():
         print(f"{new_guitar} added.\n")
         name = input("Name: ")
 
-    for i, guitar in enumerate(guitars):
-        print(f"Guitar {i + 1} {guitar.name} ({guitar.year}), worth $ {guitar.cost}")
+    # test Guitar objects
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:>8.2f}")
 
 
 main()
