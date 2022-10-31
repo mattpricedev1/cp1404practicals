18b618f13c5eb2a29ba60b94636d231bbecc5744
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-31 19:06:33 +1000

    Complete guitar_test.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -24,4 +24,4 @@ class Guitar:
 
     def is_vintage(self):
         """Determine if guitar is vintage based on age"""
-        return self.is_vintage(self) >= VINTAGE_AGE
+        return self.get_age() >= VINTAGE_AGE
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,7 +3,12 @@ from guitar import Guitar
 
 def main():
     guitar_1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    print(guitar_1.get_age())
+    guitar_2 = Guitar("Another Guitar", 2013, 12000.00)
+    print(f"{guitar_1.name} get_age() - Expected {100}. Got {guitar_1.get_age()}")
+    print(f"{guitar_2.name} get_age() - Expected {9}. Got {guitar_2.get_age()}")
+    print()
+    print(f"{guitar_1.name} is_vintage() - Expected {True}. Got {guitar_1.is_vintage()}")
+    print(f"{guitar_2.name} is_vintage() - Expected {False}. Got {guitar_2.is_vintage()}")
 
 
 if __name__ == "__main__":
