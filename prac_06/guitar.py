0e4df057dba1eac2217724fa769ad7b00510c69c
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-31 18:59:37 +1000

    Update get_age() function with datetime()

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,6 +1,6 @@
 import datetime
 
-TODAY = datetime.date.today()
+TODAY = datetime.date.today()  # get today's date
 VINTAGE_AGE = 50
 
 
@@ -8,18 +8,20 @@ class Guitar:
     """Guitar class"""
 
     def __init__(self, name="", year=0, cost=0.0):
+        """Initialise guitar"""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        """Return string formatting"""
+        """Return a string formatting of a guitar"""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
     def get_age(self):
-        year = TODAY.strftime("%Y")
-        year = int(year)
-        return year - self.year
+        """Get the age of a guitar based on current year"""
+        year = TODAY.strftime("%Y")  # get the year from today's date
+        return int(year) - self.year
 
     def is_vintage(self):
+        """Determine if guitar is vintage based on age"""
         return self.is_vintage(self) >= VINTAGE_AGE
