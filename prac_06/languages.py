acaaa5d1356558e4fa81e8ba4dc529bf6e51c0e4
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-10-31 16:46:21 +1000

    Add languages program

---
+++ prac_06/languages.py
@@ -0,0 +1,4 @@
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
