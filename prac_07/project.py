a096372552dc5749f37666e89d311a2e87937eb1
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-09 14:51:14 +1000

    Add Project class

---
+++ prac_07/project.py
@@ -0,0 +1,7 @@
+class Project:
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0, completion_percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
