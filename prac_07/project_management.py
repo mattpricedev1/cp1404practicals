0a9ba9bfa03e4f1ebb83d3db325cf0adb98d48ff
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-09 14:51:01 +1000

    Add menu with choices

---
+++ prac_07/project_management.py
@@ -0,0 +1,38 @@
+"""Project Management Program"""
+
+from project import Project
+
+MENU = "- (L)oad projects\n" \
+       "- (S)ave projects\n" \
+       "- (D)isplay projects\n" \
+       "- (F)ilter projects by date\n" \
+       "- (A)dd new project\n" \
+       "- (U)pdate project\n" \
+       "- (Q)uit"
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you for using custom-built project management software.")
+
+
+if __name__ == "__main__":
+    main()
