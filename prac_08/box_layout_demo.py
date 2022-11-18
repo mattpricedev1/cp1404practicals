2d79e7376f025f86f3c61b216b4690512e98bec7
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-18 12:09:40 +1000

    Add docstring to class and functions

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,16 +3,20 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Box Layout Demo class"""
     def build(self):
+        """Build GUI for Box Layout Demo program."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Display greeting to the user."""
         # print('test')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def clear_greet(self):
+        """Reset out label and name input."""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
