fc64ff5edd97ddba33d1d1fd52c2cfaafd5ac2c9
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-23 21:16:53 +1000

    Complete Taxi test program

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,8 @@
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi.drive(40)
+print(f"{my_taxi}\nTotal Fare: ${my_taxi.get_fare()}")
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(f"{my_taxi}\nTotal Fare: ${my_taxi.get_fare()}")
