7db21eb2ea15443b32452e361a034517fb5ed141
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-29 15:16:49 +1000

    Add tariff constants and complete computation of bill estimate

--- prac_01/Practice & Extension Work/electricity_bill.py
+++ prac_01/Practice & Extension Work/electricity_bill.py
@@ -1,9 +1,11 @@
 """Electricity bill estimator program."""
+TARIFF_11 = 0.244618
+TARIFF_31 = 0.136928
 
 print("Electricity bill estimator")
 cents_per_kwh = float(input("Enter cents per kWh: "))
 daily_kwh = float(input("Enter daily use in kWh: "))
 billing_days = int(input("Enter number of billing days: "))
-# estimated_bill =
-print("Estimated bill: {}".format(estimated_bill))
+estimated_bill = cents_per_kwh / 100 * daily_kwh * billing_days
+print("Estimated bill: ${}".format(estimated_bill))
 
