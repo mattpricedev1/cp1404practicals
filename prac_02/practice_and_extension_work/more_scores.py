a229ee64e5e065fd54cdc8cb00baab7041db9076
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-29 15:45:25 +1000

    Add random module and complete main program

--- prac_02/practice_and_extension_work/more_scores.py
+++ prac_02/practice_and_extension_work/more_scores.py
@@ -1,21 +1,28 @@
 """More scores program."""
 
+from random import randint
+
 
 def main():
-    """..."""
+    """Get number of scores and generate random scores in range of n."""
+    number_of_scores = int(input("How many scores? "))
+    for number_of_scores in range(number_of_scores):
+        score = randint(0, 100)
+        result = determine_result(score)
+        print(result)
 
 
 def determine_result(score):
-    """Check if score is valid and determine result"""
+    """Check if score is valid and determine result."""
     while score < 0 or score > 100:
         print("Invalid score")
         score = float(input("Enter score: "))
     if score >= 90:
-        result = "Excellent"
+        result = f"{score} is Excellent"
     elif score >= 50:
-        result = "Passable"
+        result = f"{score} is Passable"
     else:
-        result = "Bad"
+        result = f"{score} is Bad"
     return result
 
 
