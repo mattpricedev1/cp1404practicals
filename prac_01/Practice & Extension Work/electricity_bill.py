7c1646f897a784ce192081281ebde8aeeedc8eb3
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-29 15:25:02 +1000

    Complete electricity bill estimator program

--- prac_01/Practice & Extension Work/electricity_bill.py
+++ prac_01/Practice & Extension Work/electricity_bill.py
@@ -2,10 +2,16 @@
 TARIFF_11 = 0.244618
 TARIFF_31 = 0.136928
 
-print("Electricity bill estimator")
-cents_per_kwh = float(input("Enter cents per kWh: "))
+print("Electricity bill estimator 2.0")
+estimated_bill = 0
+tariff = input("Which tariff? 11 or 31: ")
+# cents_per_kwh = float(input("Enter cents per kWh: "))
 daily_kwh = float(input("Enter daily use in kWh: "))
 billing_days = int(input("Enter number of billing days: "))
-estimated_bill = cents_per_kwh / 100 * daily_kwh * billing_days
-print("Estimated bill: ${}".format(estimated_bill))
-
+if tariff == "11":
+    estimated_bill = TARIFF_11 * daily_kwh * billing_days
+elif tariff == "31":
+    estimated_bill = TARIFF_31 * daily_kwh * billing_days
+else:
+    print("Invalid tariff")
+print(f"Estimated bill: ${estimated_bill:.2f}")
