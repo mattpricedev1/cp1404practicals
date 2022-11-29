62a60e4d25a91abf7905ed4e86c8b04ed18a0e4f
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-11-29 15:11:15 +1000

    Add user input and output

---
+++ prac_01/Practice & Extension Work/electricity_bill.py
@@ -0,0 +1,9 @@
+"""Electricity bill estimator program."""
+
+print("Electricity bill estimator")
+cents_per_kwh = float(input("Enter cents per kWh: "))
+daily_kwh = float(input("Enter daily use in kWh: "))
+billing_days = int(input("Enter number of billing days: "))
+# estimated_bill =
+print("Estimated bill: {}".format(estimated_bill))
+
