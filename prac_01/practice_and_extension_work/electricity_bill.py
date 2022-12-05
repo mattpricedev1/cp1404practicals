e4f4081cbbf59aa773a633c0b09b2998afef407d
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2022-12-05 21:52:59 +1000

    Add dictionary for tariff options

--- prac_01/practice_and_extension_work/electricity_bill.py
+++ prac_01/practice_and_extension_work/electricity_bill.py
@@ -1,17 +1,15 @@
 """Electricity bill estimator program."""
-TARIFF_11 = 0.244618
-TARIFF_31 = 0.136928
+
+TARIFF_NUM_TO_PERCENT = {"11": 0.244618,
+                         "31": 0.136928}
 
 print("Electricity bill estimator 2.0")
 estimated_bill = 0
 tariff = input("Which tariff? 11 or 31: ")
-# cents_per_kwh = float(input("Enter cents per kWh: "))
+while tariff not in TARIFF_NUM_TO_PERCENT:
+    print("Invalid tariff")
+    tariff = input("Which tariff? 11 or 31: ")
 daily_kwh = float(input("Enter daily use in kWh: "))
 billing_days = int(input("Enter number of billing days: "))
-if tariff == "11":
-    estimated_bill = TARIFF_11 * daily_kwh * billing_days
-elif tariff == "31":
-    estimated_bill = TARIFF_31 * daily_kwh * billing_days
-else:
-    print("Invalid tariff")
+estimated_bill = TARIFF_NUM_TO_PERCENT[tariff] * daily_kwh * billing_days
 print(f"Estimated bill: ${estimated_bill:.2f}")
